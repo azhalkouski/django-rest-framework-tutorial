00153b60226510c3f181914b0151558e3af987c1
Artsiom Zhalkouski <azhalkouski@gmail.com>
2023-09-28 10:19:08 +0300

    refactoring: use HyperlinkedModelSerializer and Hyperlinking between entities instead of PrimaryKey relation

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,31 +3,53 @@ from snippets.models import Snippet
 from django.contrib.auth.models import User
 
 
-"""
-It's important to remember that ModelSerializer classes don't do anything
-particulatly magical, they are simply a shortcut for creating serializer
-classes.
-- An automatically determined set of fields
-- Simple default implementations for the create() and update() methods
-"""
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html'
+    )
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'owner',
-                  'style']
+        fields = ['url', 'id', 'highlight', 'owner', 'title', 'code',
+                  'linenos', 'language', 'style']
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Because 'snippets' is a reverse relationship on the User model, it will
-    not be included by default when using the ModelSerializer class, so we
-    need to add an explicit field for it.
-    """
-    snippets = serializers.PrimaryKeyRelatedField(many=True,
-                                                  queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True
+    )
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
+
+
+# """
+# It's important to remember that ModelSerializer classes don't do anything
+# particulatly magical, they are simply a shortcut for creating serializer
+# classes.
+# - An automatically determined set of fields
+# - Simple default implementations for the create() and update() methods
+# """
+# class SnippetSerializer(serializers.ModelSerializer):
+#     owner = serializers.ReadOnlyField(source='owner.username')
+
+#     class Meta:
+#         model = Snippet
+#         fields = ['id', 'title', 'code', 'linenos', 'language', 'owner',
+#                   'style']
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     """
+#     Because 'snippets' is a reverse relationship on the User model, it will
+#     not be included by default when using the ModelSerializer class, so we
+#     need to add an explicit field for it.
+#     """
+#     snippets = serializers.PrimaryKeyRelatedField(many=True,
+#                                                   queryset=Snippet.objects.all())
+
+#     class Meta:
+#         model = User
+#         fields = ['id', 'username', 'snippets']
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,11 +2,30 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
-import sys
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 """===== Class-based views using generic class-based views ====="""
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -19,8 +38,6 @@ class SnippetList(generics.ListCreateAPIView):
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
-    print("test")
-    print("test", file=sys.stdout)
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
